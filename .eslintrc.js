59a4aff85e82e9f18b7442932514d475fa2637ac
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2020-12-03 02:11:16 +0530

    Setup project with Yarn 2.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  extends: ['plugin:react/recommended', 'standard', 'standard-react'],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    // React Hooks rules.
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    // Allow no-multi-str.
+    'no-multi-str': 'off'
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <p>Hello World!</p>
+  )
+}
+
+export default Index
